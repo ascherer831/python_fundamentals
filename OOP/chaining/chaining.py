df16cda55718fee370c7276e7efa4b5a711a2874
Archer Scherer <ascherer831@gmail.com>
2021-05-10 14:53:38 -0500

    chaining complete

--- OOP/chaining/chaining.py
+++ OOP/chaining/chaining.py
@@ -6,12 +6,15 @@ class user: #Created class & attributes
     # Created methods for the class
     def display_user_balance(self):
         print(f"{self.name}'s account balance is ${self.balance}")
+        return self
 
     def make_deposit(self, amount):
         self.balance += amount
+        return self
 
     def make_withdrawl(self, amount):
         self.balance -= amount
+        return self
 
     def make_transfer(self, name, amount):
         amount = amount
@@ -19,29 +22,18 @@ class user: #Created class & attributes
         self.balance -= amount
         name.balance += amount
         print(f"{self.name} made a transfer of ${amount} to {recipient}")
+        return self
 
 #created instances
 koda = user("koda","koda@husky.com")
 lola = user("lola", "lola@husky.com")
 bella= user("bella", "bella@muttpup.com")
 
-koda.make_deposit(100)
-koda.make_deposit(50)
-koda.make_deposit(100)
-koda.make_withdrawl(75)
-koda.display_user_balance()
+koda.make_deposit(100).make_deposit(50).make_deposit(100).make_withdrawl(75).display_user_balance()
 
-lola.make_deposit(150)
-lola.make_deposit(100)
-lola.make_withdrawl(50)
-lola.display_user_balance()
+lola.make_deposit(150).make_deposit(100).make_withdrawl(50).display_user_balance()
 
-bella.make_deposit(400)
-bella.make_withdrawl(100)
-bella.make_withdrawl(25)
-bella.make_withdrawl(25)
-bella.display_user_balance()
+bella.make_deposit(400).make_withdrawl(100).make_withdrawl(25).make_withdrawl(25).display_user_balance()
 
-koda.make_transfer(bella, 100)
-koda.display_user_balance()
+koda.make_transfer(bella, 100).display_user_balance()
 bella.display_user_balance()

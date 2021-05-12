65a478e2b24cd603e442a45ecdd4f5e1ba735aeb
Archer Scherer <ascherer831@gmail.com>
2021-05-11 20:10:09 -0500

    user with account sensei test

--- OOP/users_with_bank_accounts/test_users_with_bank_accounts.py
+++ OOP/users_with_bank_accounts/test_users_with_bank_accounts.py
@@ -18,17 +18,18 @@ class user: #Created class & attributes
         self.account[account_number].make_deposit(amount)
         return self
 
-    # def make_withdrawl(self, amount):
-    #     self.balance -= amount
-    #     return self
+    def make_withdrawl(self, account_number, amount):
+        self.account[account_number].make_withdrawl(amount)
+        return self
 
-    # def make_transfer(self, name, amount):
-    #     amount = amount
-    #     recipient = name.name
-    #     self.balance -= amount
-    #     name.balance += amount
-    #     print(f"{self.name} made a transfer of ${amount} to {recipient}")
-    #     return self
+
+    def make_transfer(self, name, account_number, amount):
+        amount = amount
+        recipient = name.name
+        self.account[account_number].balance -= amount
+        name.account[account_number].balance += amount
+        print(f"{self.name} made a transfer of ${amount} to {recipient}")
+        return self
 
 class Bank_account:
     def __init__(self, rate=0, amount=0):
@@ -59,8 +60,8 @@ class Bank_account:
         else:
             return self
 
-savings = Bank_account(0,0)
-checking = Bank_account(0,0)
+savings = Bank_account(0.02,0)
+checking = Bank_account(0.01,0)
 
 #created instances
 koda = user("koda","koda@husky.com")
@@ -71,3 +72,6 @@ bella= user("bella", "bella@muttpup.com")
 # koda.savings_account.make_deposit(500).display_account_info()
 # lola.savings_account.make_deposit(200).display_account_info()
 koda.make_deposit(0,50).display_user_balance(0)
+koda.make_withdrawl(0, 25).display_user_balance(0)
+koda.make_withdrawl(0, 50).display_user_balance(0)
+koda.make_transfer(bella,0,25)
